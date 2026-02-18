92e706c643b2217338e213757226492341ac972a
Cgt93 <84088559+Cgt93@users.noreply.github.com>
2026-02-18 20:16:14 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,11 @@
   packageStartupMessage(paste("Welcome to the Celtic Invasive Plants database. Version", packageVersion("CelticInvasivePlantsdb"), "!"))
 }
 
+
+.onLoad <- function(libname, pkgname) {
+  file_path <- system.file("ext", "Emerald_NatDA_2024_&_Natura_2000_CIP_2025.gpkg", package = pkgname)
+  if (!file.exists(file_path)) {
+    stop(paste("No se pudo encontrar el archivo", file_path))
+  }
+  message(paste("El archivo existe:", file_path))
+}
