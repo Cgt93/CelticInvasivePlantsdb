3bd29edc24bc2da969778c994431239d879f6104
Cgt93 <84088559+Cgt93@users.noreply.github.com>
2026-02-18 21:38:34 +0000

    Update Graphs.R

--- R/Graphs.R
+++ R/Graphs.R
@@ -174,7 +174,8 @@ Taxa_Occup_CIPdb <- function(data, Scope_taxa, Scope_Area, output_name = "Taxa_O
 
 
 #Taxa Rich Graphs
-.Shp_31 <- vect(system.file("extdata", "Grids_CIP_2025.gpkg", package = "CelticInvasivePlantsdb"), layer = "Grids_Zone_31")
+.Shp_31 <- st_read(system.file("inst/extdata", "Grids_CIP_2025.gpkg", package = "CelticInvasivePlantsdb"), layer = "Grids_Zone_31")
+                
 .Protected_Areas <- st_read(system.file("extdata", "Emerald_NatDA_2024_&_Natura_2000_CIP_2025.gpkg", package = "CelticInvasivePlantsdb"))
 .mapa_celtic_fringe <- st_read(system.file("extdata", "CIP_Celtic_Fringe.gpkg", package = "CelticInvasivePlantsdb"))
 .mapa_Subprovince <- st_read(system.file("extdata", "CIP_Subprovinces.gpkg", package = "CelticInvasivePlantsdb"))
@@ -783,6 +784,7 @@ Tax_Distribution_Admin_map <- function(data, query, Taxa_Scope = "Taxa", Admin_q
     assign(map_name, map_plot, envir = .GlobalEnv)
   }
 }
+
 
 
 
