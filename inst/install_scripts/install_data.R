9992c850c127f028499582cec1c48dadca0cc6e9
Cgt93 <84088559+Cgt93@users.noreply.github.com>
2026-02-19 10:50:32 +0000

    Update install_data.R

--- inst/install_scripts/install_data.R
+++ inst/install_scripts/install_data.R
@@ -1,1 +1,8 @@
+#inst/instakl_scripts/install_data.R
+data_file = "Grids_CIP_2025.gpkg"
+data_path = file.path("inst/extdata", data_file)
 
+if (!file.exists(data_path)) {
+  url = "https:
+  download.file(url, data_path, mode = "wb")
+}
