b2e215afd10941cf13781829d7f0d1dcc207f3a8
Cgt93 <84088559+Cgt93@users.noreply.github.com>
2026-02-19 12:38:44 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,20 +2,3 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste("Welcome to the Celtic Invasive Plants database. Version", packageVersion("CelticInvasivePlantsdb"), "!"))
 }
-
-
-.onLoad <- function(libname, pkgname) {
-  file_path <- system.file("extdata", "Grids_CIP_2025.gpkg", package = pkgname)
-  if (!file.exists(file_path)) {
-    stop(paste("No se pudo encontrar el archivo", file_path))
-  }
-  message(paste("El archivo existe:", file_path))
-}
-
-
-.onLoad <- function(libname, pkgname) {
-  if (!file.exists(system.file("extdata", "Grids_CIP_2025.gpkg", package = pkgname))) {
-    git2r::lfs_pull(repo = ".")
-  }
-  # resto del código
-}
